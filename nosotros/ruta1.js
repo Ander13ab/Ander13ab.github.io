aa9bab4780d277d911884970e944caa7766edbe7
Ander13ab <andergol31@gmail.com>
2025-02-14 02:05:27 +0100

    Add files via upload

---
+++ nosotros/ruta1.js
@@ -0,0 +1,30 @@
+{
+    "@context": "https://schema.org",
+    "@type" : "BreadcrumbList",
+    "ItemListElement": [
+        {
+            "@type" : "ListItem",
+            "position" : 1,
+            "name" : "Inicio",
+            "item" : "https://example.com"
+        },
+        {
+            "@type" : "ListItem",
+            "position" : 2,
+            "name" : "pag-2",
+            "item" : "https://example.com/pag2"
+        },
+        {
+            "@type" : "ListItem",
+            "position" : 3,
+            "name" : "pag-3",
+            "item" : "https://example.com/pag2/pag3"
+        },
+        {
+            "@type" : "ListItem",
+            "position" : 4,
+            "name" : "pag-4",
+            "item" : "https://example.com/pag2/pag3/pag4"
+        }
+    ]
+}
